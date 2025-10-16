62d611348055095a3a9bac163ad3edcec9541222
adeel ranjha <adeelranjha564@gmail.com>
2025-10-16 20:22:18 +0000

    gs://studio-9192899443-477f5.firebasestorage.app

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-9192899443-477f5",
   "appId": "1:1073933432849:web:e1559e82bfdba4512ed9b0",
+  "storageBucket": "studio-9192899443-477f5.firebasestorage.app",
   "apiKey": "AIzaSyBe_Nsyxyf_vv0KgmFkFL5KATS5d9VUF88",
   "authDomain": "studio-9192899443-477f5.firebaseapp.com",
   "measurementId": "",
